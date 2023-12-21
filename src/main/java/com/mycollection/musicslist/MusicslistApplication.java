a119c9d207d3d86a694732cdb920fd8edf9127fb
Luis <luissecundes@gmail.com>
2023-12-21 07:13:09 -0300

    Projeto criado

---
+++ src/main/java/com/mycollection/musicslist/MusicslistApplication.java
@@ -0,0 +1,13 @@
+package com.mycollection.musicslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycollection/musicslist/MusicslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycollection.musicslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
